4cb75a75aed3b03feeb1febad7b6e521b03f2188
Benedikt S. Vogler <developer@benediktsvogler.com>
2021-05-16 21:27:53 +0200

    fix crash

--- insimou/insimou/PlaceCellLayer.cpp
+++ insimou/insimou/PlaceCellLayer.cpp
@@ -17,22 +17,24 @@ PlaceCellLayer::PlaceCellLayer(std::vector<float> min,
                                 distance_pc(min.size())
 {
     //calculate total number of positions
-    this->numPos = res[0];
-    this->distance_pc = std::vector<float>();
     for (int dim=0; dim<this->distance_pc.size(); ++dim){
         this->distance_pc[dim] = abs((max[dim] - min[dim])) / (res[dim] - 1);
         std::cout << this->distance_pc[dim] <<", ";
-        numPos *= res[dim];
+        if (dim==0){
+            numPos = res[dim];
+        } else {
+            numPos *= res[dim];
+        }
     }
     this->positions.reserve(numPos);
     //distance between centers covered by the pc per dimension
 
     //fill list of all equidistance placed neurons
-    position  pos = position(min.size());
+    position pos = position(min.size());//current position starting with the zero position
     for (int i=0; i<numPos; ++i){
         int currentDimIterator=0;
         this->positions.push_back(pos);//alternatively set with = and use resize in init
-        for (int dim=0; dim < this->positions.size(); ++dim){
+        for (int dim=0; dim < min.size(); ++dim){
             //scale
             this->positions[i][dim] = pos[dim] * this->distance_pc[dim] + min[dim];
             //std::cout << pos[dim] << "("<< this->positions[i][dim] << ")"<<", ";
@@ -82,7 +84,7 @@ std::vector<float> PlaceCellLayer::activation(position observation){
         }
     }
     //normalize and exp so that near neurosn are exponentialyl weighted more
-    std::cout <<"sum scaleddistance"<<distancesum<<std::endl;
+    std::cout <<"sum scaleddistance "<<distancesum<<std::endl;
     for (auto neurondist : scaleddistance) {
         //std::cout << neurondist << ", ";
         neurondist = exp(-neurondist / distancesum);
